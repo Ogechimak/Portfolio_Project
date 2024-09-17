2c27b132aafaa943cd3743a0c529d7bebba22bb2
Ogechimak <ogechimac111@gmaail.com>
2024-09-17 13:19:12 +0100

    update

--- my-react-portfolio/src/scenes/Landing.jsx
+++ my-react-portfolio/src/scenes/Landing.jsx
@@ -56,8 +56,7 @@ const Landing = ({ setSelectedPage }) => {
           </p>
 
           <p className="mt-10 mb-7 text-sm text-center md:text-start">
-            Adipiscing arcu, in aliquam fringilla cursus. Elit arcu elementum
-            viverra malesuada sem ac faucibus dolor. Sagittis scelerisque.
+          I am a software Engineer/Frontend Developer and Chemist from Enugu State, based in Abuja,Nigeria.
           </p>
         </motion.div>
 
